d92b36cbc6b207a7722596bb40bde9d432c9643d
Sebastian Kihle <sebbek8@hotmail.com>
2019-10-21 12:53:13 +0200

    finished myrand file and made empty walker class file. finished ListRand

--- exersices/ex04/myrand.py
+++ exersices/ex04/myrand.py
@@ -8,10 +8,9 @@ class LCGRand:
     def __init__(self, seed):
         self.seed = seed
         self.counter = 0
-        self.r = [0]
+        self.r = [seed]
 
     def rand(self):
-        self.r[0] = self.seed
         self.r.append("placeholder value")
         a = 7**5
         m = 2**31 - 1
@@ -23,6 +22,10 @@ class LCGRand:
 class ListRand:
     def __init__(self, numbers):
         self.numbers = numbers
+        self.counter = -1
 
     def rand(self):
-        return self.numbers.pop()
+        self.counter += 1
+        if self.counter > len(self.numbers) - 1:
+            raise RuntimeError
+        return self.numbers[self.counter]
---
+++ exersices/ex04/walker.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'sebastian kihle'
+__email__ = 'sebaskih@nmbu.no'
+
+class Walker:
+    def __init__(self):
+        pass
