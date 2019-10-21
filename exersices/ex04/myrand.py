9d4588cbcd58c9f363e99139b9257c74e6f80dbe
Sebastian Kihle <sebbek8@hotmail.com>
2019-10-21 11:48:18 +0200

    finished LCGRand method

--- exersices/ex04/myrand.py
+++ exersices/ex04/myrand.py
@@ -7,13 +7,17 @@ __email__ = 'sebaskih@nmbu.no'
 class LCGRand:
     def __init__(self, seed):
         self.seed = seed
+        self.counter = 0
+        self.r = [0]
 
     def rand(self):
-        r = self.seed
+        self.r[0] = self.seed
+        self.r.append("placeholder value")
         a = 7**5
         m = 2**31 - 1
-        r = a * r % m
-        return r
+        self.r[self.counter + 1] = a * self.r[self.counter] % m
+        self.counter += 1
+        return self.r
 
 
 class ListRand:
