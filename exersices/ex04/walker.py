86988976c8f70de6aba4623a4abcf1940ba534bf
Sebastian Kihle <sebbek8@hotmail.com>
2019-10-22 11:23:52 +0200

    finished the main function for walker

--- exersices/ex04/walker.py
+++ exersices/ex04/walker.py
@@ -36,10 +36,52 @@ class Walker:
 
 if __name__ == "__main__":
     w1list = []
+    w2list = []
+    w3list = []
+    w4list = []
+    w5list = []
+    w6list = []
+    w7list = []
     for _ in range(5):
         w1 = Walker(0, 1)
         while not w1.position == w1.home:
             w1.move()
         w1list.append(w1.get_steps())
-    print(w1list)
 
+        w2 = Walker(0, 2)
+        while not w2.position == w2.home:
+            w2.move()
+        w2list.append(w2.get_steps())
+
+        w3 = Walker(0, 5)
+        while not w3.position == w3.home:
+            w3.move()
+        w3list.append(w3.get_steps())
+
+        w4 = Walker(0, 10)
+        while not w4.position == w4.home:
+            w4.move()
+        w4list.append(w4.get_steps())
+
+        w5 = Walker(0, 20)
+        while not w5.position == w5.home:
+            w5.move()
+        w5list.append(w5.get_steps())
+
+        w6 = Walker(0, 50)
+        while not w6.position == w6.home:
+            w6.move()
+        w6list.append(w6.get_steps())
+
+        w7 = Walker(0, 100)
+        while not w7.position == w7.home:
+            w7.move()
+        w7list.append((w7.get_steps()))
+
+    print("Distance:   1 -> Path lengths: {0}".format(w1list))
+    print("Distance:   2 -> Path lengths: {0}".format(w2list))
+    print("Distance:   5 -> Path lengths: {0}".format(w3list))
+    print("Distance:   10 -> Path lengths: {0}".format(w4list))
+    print("Distance:   20 -> Path lengths: {0}".format(w5list))
+    print("Distance:   50 -> Path lengths: {0}".format(w6list))
+    print("Distance:   100 -> Path lengths: {0}".format(w7list))
