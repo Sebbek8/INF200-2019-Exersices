97f9089b17d243bd403ec510e102475f4df439d3
Sebastian Kihle <sebbek8@hotmail.com>
2019-10-23 10:48:40 +0200

    Added strings with explanations to Walker

--- exersices/ex04/walker.py
+++ exersices/ex04/walker.py
@@ -10,38 +10,49 @@ class Walker:
     def __init__(self, startpoint, home):
         self.startpoint = startpoint
         self.home = home
+        """Starts with 0 steps"""
         self.steps = 0
         self.position = startpoint
 
     def move(self):
+        """Uses randint to have 50% chance of moving backwards and forwards"""
         step = random.randint(1, 2)
         if step == 2:
             step = -1
         self.steps += 1
+        """Counts the total amount of steps"""
         self.position += step
         self.is_at_home()
         return self.get_steps()
 
     def is_at_home(self):
+        """Check if person is at home"""
         if self.position == self.home:
             return True
         else:
             return False
 
     def get_position(self):
+        """Gets current position"""
         return self.position
 
     def get_steps(self):
+        """Gets current amount of steps"""
         return self.steps
 
 
 if __name__ == "__main__":
 
+    """List of all the distances"""
     distances = [1, 2, 5, 10, 20, 50, 100]
+
+    """Double for-loop to do each distance 5 times"""
     for distance in distances:
+        """Defines temporary list that contains the 5 values, and resets"""
         templist = []
         for _ in range(5):
             w = Walker(0, distance)
+            """Walk until you are home"""
             while not w.position == w.home:
                 w.move()
             templist.append(w.get_steps())
